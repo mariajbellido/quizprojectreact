97a04268b632429dbb733268c6b0bd2acc959df1
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-06 13:44:15 +0100

    Creating all different components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Quiz from './components/Quiz';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Quiz />
   </React.StrictMode>,
   document.getElementById('root')
 );
