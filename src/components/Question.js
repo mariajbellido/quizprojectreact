9c6c728525f6b0d364699606ead60873bb79d186
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-06 18:53:45 +0100

    Rendering components mark-up

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,18 @@
+import Answer from './Answer';
+
 const Question = () => {
-    return <div>
-        Question
-    </div>
+    return (
+        <div>
+            <div className="question">Text of our Question</div>
+            <div className="answers">
+                <Answer/>
+                <Answer/>
+                <Answer/>
+                <Answer/>
+            </div>
+
+        </div>
+    )
 };
 
 export default Question;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,20 @@
+import Question from "./Question";
+
+
 const Quiz = () => {
-    return <div>
-        Quiz
-    </div>
+    return (
+        <div className="quiz">
+            <div>
+                <div>
+                    <div className="score">
+                        Question 1 / 10
+                    </div>
+                </div>
+                <Question />
+                <div className="next-button">Next Question</div>
+            </div>
+        </div>
+        )
 };
 
 export default Quiz;
