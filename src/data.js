4d2a7e326161b6c3000e39c4cf46d553a155a728
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-06 13:37:29 +0100

    Adding some data and basic styling

---
+++ src/data.js
@@ -0,0 +1,106 @@
+const data = [
+
+    {
+        question: "Children, at what time are you going ... school today?",
+        incorrectAnswers: [
+            "at",
+            "to the",
+            "at the"
+        ],
+        correctAnswer: "to"
+    },
+
+    {
+        question: "She has been waiting for her boyfriend ... October",
+        incorrectAnswers: [
+            "for",
+            "already",
+            "from"
+        ],
+        correctAnswer: "since"
+    },
+
+    {
+        question: "We haven't got ... money to buy a new car yet",
+        incorrectAnswers: [
+            "too many",
+            "too much",
+            "enough"
+        ],
+        correctAnswer: "enough"
+    },
+
+    {
+        question: "The little girl had a cup of tea in ... hands",
+        incorrectAnswers: [
+            "his",
+            "your",
+            "the"
+        ],
+        correctAnswer: "her"
+    },
+
+    {
+        question: "How old is your brother, Susan? He ... ten yesterday",
+        incorrectAnswers: [
+            "is",
+            "has",
+            "had"
+        ],
+        correctAnswer: "was"
+    },
+
+    {
+        question: "How ... are you going to be in Madrid, Mr. Smith?",
+        incorrectAnswers: [
+            "much",
+            "time",
+            "many"
+        ],
+        correctAnswer: "long"
+    },
+
+    {
+        question: "There ... children in the park, weren't there?",
+        incorrectAnswers: [
+            "was a lot",
+            "were a lot",
+            "was lots of"
+        ],
+        correctAnswer: "were a lot"
+    },
+
+    {
+        question: "You can speak Swedish better than your uncle, ...?",
+        incorrectAnswers: [
+            "can you",
+            "don't you",
+            "speak you"
+        ],
+        correctAnswer: "can't you"
+    },
+
+    {
+        question: "That old lady drinks too much, ...?",
+        incorrectAnswers: [
+            "does she",
+            "isn't it",
+            "drinks she"
+        ],
+        correctAnswer: "doesn't she"
+    },
+
+    {
+        question: "How ... are you going to give Martha for her work",
+        incorrectAnswers: [
+            "-",
+            "many",
+            "money"
+        ],
+        correctAnswer: "much"
+    },
+
+
+]
+
+export default data;
